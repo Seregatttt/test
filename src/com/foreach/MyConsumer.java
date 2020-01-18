56bbf5567795773ba223512d79644fb07e6fd0ff
seregatttt <seregatttt@mail.ru>
2020-01-18 20:51:20 +0300

    br6 123

--- src/com/foreach/MyConsumer.java
+++ src/com/foreach/MyConsumer.java
@@ -4,7 +4,7 @@ import java.util.function.Consumer;
 
 //Реализация интерфейса Consumer master 12
 
-//Реализация интерфейса Consumer br6 12
+//Реализация интерфейса Consumer br6 123
 class MyConsumer implements Consumer<Integer> {
 	
 	public void accept(Integer t) {
